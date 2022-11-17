cc54a655517f7b022811005bc8ad0085398664e1
clever <cleverumuhuza@gmail.com>
2022-11-17 15:31:47 +0200

    add colors in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,16 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#4589ff",
+        darkblue: "#0f62fe",
+        secondary: "#161616",
+        gray: "#6f6f6f",
+        darkgray: "#8d8d8d",
+        lightgray: "#393939",
+      },
+    },
   },
   plugins: [],
-}
+};
