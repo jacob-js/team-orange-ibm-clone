4f3d2044789eea6c74b28ba283aa1373997dca21
Jacob Dev <mercihabam@gmail.com>
2022-11-17 16:44:08 +0200

    create navbar

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,8 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import Navbar from '../components/Layout/Navbar'
 export default function Home() {
   return (
     <div>
-      <Navbar />
-    </div>
+      </div>
   )
 }
