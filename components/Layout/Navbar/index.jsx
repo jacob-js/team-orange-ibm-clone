c44009e0cd2563cbb800b561ceb37e4f6454a612
Jacob Dev <mercihabam@gmail.com>
2022-11-17 16:42:52 +0200

    create navbar

---
+++ components/Layout/Navbar/index.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function Navbar() {
+    const items = ['Über uns', 'Wie wir arbeiten', 'Services', 'Projekte', 'Branchen', 'Karriere', 'What’s up?']
+  return (
+    <div>
+        <div className="flex justify-between bg-secondary text-white px-6 py-3 fixed top-0 left-0 right-0">
+            <div className="flex items-center gap-8">
+                <div className="text-[23px]">IBM <strong>iX</strong></div>
+                <div className="flex gap-8">
+                    {
+                        items.map((item, i) =>(
+                            <div className="cursor-pointer hover:text-primary transition-all duration-150" key={i}>{item}</div>
+                        ))
+                    }
+                </div>
+            </div>
+            <div className="">EN</div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Navbar from '../components/Layout/Navbar'
 export default function Home() {
   return (
     <div>
+      <Navbar />
     </div>
   )
 }
